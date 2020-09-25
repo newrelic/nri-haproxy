494cab0b8d0e15344c26392d2a18f30470a55771
Camden Cheek <camden@ccheek.com>
2020-09-25 10:38:05 -0400

    Update Makefiles

--- Makefile-docker.mk
+++ Makefile-docker.mk
@@ -3,10 +3,10 @@ INTEGRATION_DIR := nri-$(INTEGRATION)
 docker-fmt:
 	@echo "=== $(INTEGRATION) === [ docker-fmt ]: Running gofmt in Docker..."
 	@echo "Using Docker image $(DOCKER_IMAGE)"
-	@docker run -it --rm -v $(PWD):/go/src/github.com/newrelic/$(INTEGRATION_DIR) -w /go/src/github.com/newrelic/$(INTEGRATION_DIR) $(DOCKER_IMAGE) "gofmt" "-s" "-w" "."
+	@docker run -it --rm -v $(PWD):/go/src/github.com/newrelic/$(INTEGRATION_DIR) -w /go/src/github.com/newrelic/$(INTEGRATION_DIR) $(DOCKER_IMAGE) "gofmt" "-s" "-w" "./src"
 docker-make:
 	@echo "=== $(INTEGRATION) === [ docker-fmt ]: Running make in Docker..."
 	@echo "Using Docker image $(DOCKER_IMAGE)"
 	@docker run -it --rm -v $(PWD):/go/src/github.com/newrelic/$(INTEGRATION_DIR) -w /go/src/github.com/newrelic/$(INTEGRATION_DIR) $(DOCKER_IMAGE) "make"
-	
-.PHONY: docker-fmt docker-make 
+
+.PHONY: docker-fmt docker-make
